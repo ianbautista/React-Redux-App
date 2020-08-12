eddd848f430f15d25151c6003437786bea11606b
ianbautista <ianfbautista@gmail.com>
2020-08-12 16:04:11 -0700

    Set up "empty" reducer and initial state

---
+++ app/src/store/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+	insult: "",
+	isLoading: false,
+	error: "",
+};
+
+export const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
