32cbb8e3ee29b2f76d464743ca50a6deb04f18c4
ianbautista <ianfbautista@gmail.com>
2020-08-12 16:22:41 -0700

    added applyMiddleware and thunk

--- app/src/index.js
+++ app/src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-import { reducer } from "./store";
+import { reducer } from "./store/reducers";
 
 import App from "./App";
 
